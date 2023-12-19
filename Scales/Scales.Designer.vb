4e3cfca0d1c4101a9a584067a3669188ebd4c262
TD <TD@DESKTOP-T1U99UQ>
2023-12-19 16:40:27 +0500

    Add project files.

---
+++ Scales/Scales.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Scales
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Scales))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.txtScale = New System.Windows.Forms.TextBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.lblScale = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 3000
+        '
+        'txtScale
+        '
+        Me.txtScale.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtScale.Location = New System.Drawing.Point(90, 8)
+        Me.txtScale.Name = "txtScale"
+        Me.txtScale.Size = New System.Drawing.Size(125, 21)
+        Me.txtScale.TabIndex = 0
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.DiscardNull = True
+        '
+        'lblScale
+        '
+        Me.lblScale.AutoSize = True
+        Me.lblScale.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScale.Location = New System.Drawing.Point(9, 11)
+        Me.lblScale.Name = "lblScale"
+        Me.lblScale.Size = New System.Drawing.Size(62, 15)
+        Me.lblScale.TabIndex = 1
+        Me.lblScale.Text = "Message:"
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(163, 35)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Close"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(90, 35)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(67, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Connect"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Scales
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(229, 67)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblScale)
+        Me.Controls.Add(Me.txtScale)
+        Me.Enabled = False
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Scales"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Maheen Textile Mills (pvt) Ltd."
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents txtScale As System.Windows.Forms.TextBox
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents lblScale As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Scales/Scales.vb
@@ -0,0 +1,127 @@
+﻿Imports System.ComponentModel
+Imports System.IO.Ports
+Imports System.Text
+Imports System.Threading
+
+
+'Module Program
+'    <STAThread>
+'    Public Sub Main()
+'        Application.EnableVisualStyles()
+'        Application.SetCompatibleTextRenderingDefault(False)
+'        Application.Run(New Scales())
+'    End Sub
+'End Module
+
+
+Public Class Scales
+    Private WithEvents SerialPortWorker As New BackgroundWorker()
+    Private buffer As New StringBuilder()
+
+
+
+
+
+    Private Sub Scales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        SerialPort1.PortName = "COM1"
+        SerialPort1.BaudRate = 9600
+        SerialPort1.Parity = Parity.None
+        SerialPort1.DataBits = 8
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.ReadTimeout = 1000
+
+        Try
+            If Not SerialPort1.IsOpen Then
+                SerialPort1.Open()
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error opening serial port: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Me.Close()
+            Return
+        End Try
+
+        SerialPortWorker.WorkerSupportsCancellation = True
+        SerialPortWorker.RunWorkerAsync()
+    End Sub
+
+    Private Sub SerialPortWorker_DoWork(sender As Object, e As DoWorkEventArgs) Handles SerialPortWorker.DoWork
+        While Not SerialPortWorker.CancellationPending
+            Try
+
+                Dim receivedData As String = SerialPort1.ReadExisting()
+                buffer.Append(receivedData)
+
+
+                While buffer.ToString().Contains("=")
+                    Dim separatorIndex As Integer = buffer.ToString().IndexOf("=")
+                    Dim message As String = buffer.ToString().Substring(0, separatorIndex).Trim()
+
+
+                    Dim rmessage As String = New String(message.Reverse().ToArray())
+
+                    If message.StartsWith("=") Then
+                        Dim weightValue As String = rmessage.Substring(1).Trim()
+                        UpdateUIText(weightValue)
+
+                        ' UpdateUIText("xyz")
+                    Else
+                        'My.Computer.Clipboard.SetText("some text")
+
+                        Me.Invoke(Sub()
+                                      Try
+                                          ' Ensure that rmessage is not null before setting it as clipboard text
+                                          If rmessage IsNot Nothing Then
+                                              Clipboard.SetText(rmessage)
+                                          Else
+                                              ' Handle the case where rmessage is null
+                                              UpdateUIText("Unexpected data format: rmessage is null")
+                                          End If
+                                      Catch ex As Exception
+                                          ' Log or handle the exception
+                                          UpdateUIText("Success")
+                                      End Try
+                                  End Sub)
+
+                        'UpdateUIText(rmessage)
+
+                    End If
+
+                    buffer.Remove(0, separatorIndex + 1)
+                End While
+            Catch ex As TimeoutException
+
+                UpdateUIText("Timeout exception: " & ex.Message)
+            Catch ex As Exception
+
+                UpdateUIText("Error reading weight: " & ex.Message)
+            End Try
+
+        End While
+    End Sub
+
+    Private Sub UpdateUIText(text As String)
+        ' Update the UI with the specified text
+        If txtScale.InvokeRequired Then
+            ' If not on the UI thread, invoke the method on the UI thread
+            txtScale.Invoke(Sub() UpdateUIText(text))
+        Else
+            ' Update the UI directly
+            txtScale.Text = text
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SerialPortWorker.CancelAsync()
+        SerialPort1.Close()
+        Application.Exit()
+    End Sub
+
+    Private Sub Scales_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        SerialPortWorker.CancelAsync()
+        SerialPort1.Close()
+    End Sub
+
+
+
+End Class
